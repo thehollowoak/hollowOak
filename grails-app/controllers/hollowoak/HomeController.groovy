f8d50ceb3658516925a98bc08fb6c62e0d623b75
Lydia White <thehollowoak@gmail.com>
2018-01-08 11:37:16 -0500

    Added cart with session

--- grails-app/controllers/hollowoak/HomeController.groovy
+++ grails-app/controllers/hollowoak/HomeController.groovy
@@ -11,4 +11,20 @@ class HomeController {
         def item = Item.findById(params.id)
         render(view: 'view', model: [item: item])
     }
+
+    def cart() {
+        def items = session.items
+        render(view: 'cart', model: [items: items])
+    }
+
+    def addToCart() {
+        if (session.items) {
+            session.items += Item.findById(params.id)
+        }
+        else {
+            session.items = [Item.findById(params.id)]
+        }
+        flash.message = "Item added to cart"
+        redirect(action: 'view', params: [id: params.id])
+    }
 }
