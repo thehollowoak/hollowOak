6a83e3d59f00ff0451dc1353217075a43b428c98
Lydia White <thehollowoak@gmail.com>
2018-02-07 19:51:38 -0500

    Symbol enum

--- grails-app/assets/javascripts/game.js
+++ grails-app/assets/javascripts/game.js
@@ -6,10 +6,13 @@ class Game {
     constructor(rows, cols) {
         this.rows = rows;
         this.cols = cols;
+        this.grid = new Array(rows);
         for(var row = 0; row < this.rows; row++) {
+            this.grid[row] = new Array(cols);
             for(var col = 0; col < this.cols; col++) {
-                var symbol = (row == 0 || row == this.rows-1 || col == 0 || col == this.cols-1 ? '#' : '.');
-                new Space(row, col, symbol);
+                var symbol = (row == 0 || row == this.rows-1 || col == 0 || col == this.cols-1 ? Symbol.WALL : Symbol.EMPTY);
+                this.grid[row][col] = symbol;
+                $("#row"+row+"col"+col).text(symbol);
             }
         }
     }
@@ -26,24 +29,27 @@ class Space {
 
 class Charactor {
     constructor(row, col) {
-        this.row = row;
-        this.col = col;
-        this.symbol = '&';
-        $("#row"+this.row+"col"+this.col).text('&');
+        this.space = new Space(row, col, Symbol.CHARACTOR);
     }
     move(y, x) {
-        $("#row"+this.row+"col"+this.col).text('.');
-        this.row += y;
-        this.col += x;
-        $("#row"+this.row+"col"+this.col).text('&');
+        $("#row"+this.space.row+"col"+this.space.col).text(game.grid[this.space.row][this.space.col]);
+        this.space.row += y;
+        this.space.col += x;
+        $("#row"+this.space.row+"col"+this.space.col).text(Symbol.CHARACTOR);
     }
 }
+
+var Symbol = {
+    EMPTY: " ",
+    WALL: "#",
+    CHARACTOR: "&",
+  };
 
 $(document).ready(function(){
     game = new Game(6, 15);
     mc = new Charactor(1,1);
     $("button").click(function() {
-        mc.move(1,1);
+        mc.move(1,1); 
     });
 });
 
